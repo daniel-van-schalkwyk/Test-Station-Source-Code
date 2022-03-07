9e34c841ed3c9abb835623c7ff2c3cff6e878c14
daniel-van-schalkwyk <pdvs14@gmail.com>
2022-03-07 12:09:48 +0200

    Added a new source file for inlet controller

---
+++ InletWaterControl/src/wifiDetails.h
@@ -0,0 +1,9 @@
+#ifndef wifiDetails_h
+#define wifiDetails_h
+
+// Secret Wifi details
+char ssid[] = {"Naspers"};   // your network SSID (name)
+char pass[] = {"googleDevelop"};   // your network password (use for WPA, or use as key for WEP)
+
+
+#endif 
