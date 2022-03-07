a7874495c7ab1a1bb698baac54e7822eb8143cc9
daniel-van-schalkwyk <pdvs14@gmail.com>
2022-03-07 11:39:54 +0200

    Added a functional script that imports the class library

---
+++ InterfaceScript/geyserInterfaceScript/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using geyserTestStationAPI;
+
+namespace geyserInterfaceScript
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var geyserObject = new geyserTestStationAPI.geyserTestStationAPI();
+            String serialPortName = "COM8";
+            geyserObject.OpenSerialPort(serialPortName);        
+            // ** Add get and set messages here ** 
+        }
+    }
+}
